16ba9d89bbe0d38167a63a7ad6ffc2cb67267583
Vitaliy <vitaliyufc@gmail.com>
2025-08-28 15:43:38 +0300

    Fixed project for API

--- src/test/java/mate/academy/springboot/web/ApplicationTests.java
+++ src/test/java/mate/academy/springboot/web/ApplicationTests.java
@@ -2,8 +2,10 @@ package mate.academy.springboot.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
